51bcb0494a10fa78ea8b076d0570accaf35ccd51
Legoshi1999 <127850771+Legoshi1999@users.noreply.github.com>
2023-11-20 20:11:40 +0100

    fehler bei konfliktgelöst vergessen zeichen zu löschen behoben

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,12 +5,9 @@ public class Main {
 
         Car car1 = new Car("red", "BMW", 5);
         Car car2 = new Car("blue", "Audi", 3);
-<<<<<<< HEAD
         Car car3 = new Car("pink", "VW", 3);
 
         Car car4 = new Car("yellow", "Renault", 4);
-=======
->>>>>>> merge-conflict
 
         factory.buildCar(car1);
         factory.buildCar(car2);
